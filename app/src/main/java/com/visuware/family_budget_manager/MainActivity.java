f183cdfea337d66a77b866cec5da3f3d5e9f275f
vvboss <arumugamprema69@gmail.com>
2025-10-12 22:03:07 +0530

    modify splash icon color, app_name

--- app/src/main/java/com/visuware/family_budget_manager/MainActivity.java
+++ app/src/main/java/com/visuware/family_budget_manager/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.splashscreen);
 
-                //Toast message
                 Toast.makeText(getApplicationContext(),"Welcome to ViSuWare", Toast.LENGTH_LONG).show();
         }
 }
