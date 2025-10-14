8be617f0c2dec6cdd2a96058c37955c9eb3ceecc
vvboss <arumugamprema69@gmail.com>
2025-10-14 21:17:46 +0530

    essential changes

--- app/src/main/java/com/visuware/family_budget_manager/MainActivity.java
+++ app/src/main/java/com/visuware/family_budget_manager/MainActivity.java
@@ -1,7 +1,10 @@
 package com.visuware.family_budget_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.window.SplashScreen;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.whospent);
+                setContentView(R.layout.activity_main);
 
-                Toast.makeText(getApplicationContext(),"Welcome to ViSuWare", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Welcome to visuware", Toast.LENGTH_LONG).show();
         }
 }
